e74d782ef1923066cfbb28df06630067f878ee0a
Usov Nikita <usov107@gmail.com>
2023-05-30 19:02:03 +0700

    Initial Commit

---
+++ Food-delivery-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food-delivery-app
+//
+//  Created by Nikita Usov on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
